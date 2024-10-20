ce63f53cf972f71366184d2d48e9bb344f25ab32
Gmmarinho07 <maiamarinho66@gmail.com>
2024-10-20 19:59:03 +0000

    .

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World")
+falta_mel = False
+falta_pao = True
+
+
+if falta_mel and falta_pao:
+    print("Precisamos ir ao mercado")
+
+if falta_mel or falta_pao:
+    print("Também precisamos ir ao mercado")
+
