ba4f11d99bec3b362d15e0ad63f18712d66604b3
Gmmarinho07 <maiamarinho66@gmail.com>
2024-10-27 15:26:06 -0300

    Novo cronometro

---
+++ funções.py
@@ -0,0 +1,23 @@
+# funções
+def soma(x, y):
+    vsoma = x + y
+    return vsoma
+    
+
+
+#print(soma(x = 1000, y = 2000))
+
+
+def par(numero):
+    return numero % 2 == 0
+
+print(par(4))
+
+
+def menos(x, y):
+    return x - y
+
+
+print(menos(3,5))
+
+
